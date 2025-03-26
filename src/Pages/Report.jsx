38f98e6f2f5f009ccb01cdcc4030562eece44537
anbuleo <anbuliyon@gmail.com>
2025-03-26 00:24:36 -0700

    home Page

--- src/Pages/Report.jsx
+++ src/Pages/Report.jsx
@@ -74,7 +74,7 @@ const getUser = async()=>{
           <p className="text-4xl font-bold underline text-rose-400 ">Plan Wise</p>
 <div className="grid grid-cols-2 md:grid-cols-4 gap-4 pt-4">
 {planwise && planwise?.map((e,i)=>{
-            return     <div key={i}  className="p-4 bg-gradient-to-br from-fuchsia-600 to-orange-600 text-white  shadow rounded text-center">
+            return     <div key={i}  className="p-4  bg-[#251047]  text-white  shadow rounded text-center">
               <p className="text-xl font-bold">Plan Name : {e.planName} </p>
               {/* <p className="text-gray-600">totalAmountDue:  ₹{e.totalAmountDue}</p> */}
            
